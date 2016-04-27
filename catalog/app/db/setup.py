27664b7b57dfce7160277d7b7b754e9b525bb027
Wilbert Sequeira <wilbertcr@gmail.com>
2016-04-26 17:30:39 -0700

    Fixed:
    Instead of using relative paths for the files that are loaded. I am using the full path.

--- catalog/app/db/setup.py
+++ catalog/app/db/setup.py
@@ -1,11 +1,14 @@
 import database
 from subprocess import call
+from app.config.config import Config
 
+setup_path = Config.BASE_DIR+"/app/db/setup_db.sh"
 # Script sets up password and database in the database server.
-call("./setup_db.sh")
+call(setup_path)
 
 # Initiate database tables(SQLAlchemy)
 database.init_db()
 
+populate_path = Config.BASE_DIR+"/app/db/populate_db.py"
 # Populates database.
-execfile("./populate_db.py")
+execfile(populate_path)
